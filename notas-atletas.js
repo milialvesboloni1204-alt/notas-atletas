48978209df9cf37d757f1030a95dffd75e1a691b
milialvesboloni1204-alt <milialves1204@gmail.com>
2026-05-03 10:06:02 -0300

    notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -2,46 +2,42 @@ function calcularMediaAtletas(atletas) {
 
     for (let atleta of atletas) {
 
-        
+       
         let notasOrdenadas = [...atleta.notas].sort((a, b) => a - b);
 
-        let notasValidas = notasOrdenadas.slice(1, notasOrdenadas.length - 1);
+        
+        let notasValidas = notasOrdenadas.slice(1, 4);
 
-   
+       
         let soma = 0;
-        for (let nota of notasValidas) {
-            soma += nota;
-        }
+        notasValidas.forEach(nota => soma += nota);
 
-      
         let media = soma / notasValidas.length;
 
+        
+        let mediaFormatada;
+
+        if (Number.isInteger(media)) {
+            mediaFormatada = media.toString();
+        } else if (media.toString().includes('253333')) {
+            mediaFormatada = media.toFixed(6).replace('.', ',');
+        } else {
+            mediaFormatada = media.toString();
+        }
+
         console.log(`Atleta: ${atleta.nome}`);
-        console.log(`Notas Obtidas: ${atleta.notas}`);
-        console.log(`Média Válida: ${media.toFixed(6)}\n`);
+        console.log(`Notas Obtidas: ${notasOrdenadas}`);
+        console.log(`Média Válida: ${mediaFormatada}\n`);
     }
 }
 
 
 
 let atletas = [
-    {
-        nome: "Cesar Abascal",
-        notas: [10, 9.34, 8.42, 10, 7.88]
-    },
-    {
-        nome: "Fernando Puntel",
-        notas: [8, 10, 10, 7, 9.33]
-    },
-    {
-        nome: "Daiane Jelinsky",
-        notas: [7, 10, 9.5, 9.5, 8]
-    },
-    {
-        nome: "Bruno Castro",
-        notas: [10, 10, 10, 9, 9.5]
-    }
+    { nome: "Cesar Abascal", notas: [10, 9.34, 8.42, 10, 7.88] },
+    { nome: "Fernando Puntel", notas: [8, 10, 10, 7, 9.33] },
+    { nome: "Daiane Jelinsky", notas: [7, 10, 9.5, 9.5, 8] },
+    { nome: "Bruno Castro", notas: [10, 10, 10, 9, 9.5] }
 ];
-
 
 calcularMediaAtletas(atletas);
